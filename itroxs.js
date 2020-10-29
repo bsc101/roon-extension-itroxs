3f0d55c93923ad70ffd2454eced9d667df569fb6
Boris Schaedler <bsc@bsc101.eu>
2020-10-29 17:12:01 +0100

    1.0.8

--- itroxs.js
+++ itroxs.js
@@ -47,7 +47,7 @@ var instance = "";
 var instance_display_name = "";
 
 var ext_id      = 'com.bsc101.itroxs';
-var ext_version = '1.0.7';
+var ext_version = '1.0.8';
 
 var subscribe_delay = 1000;
 var subscribe_timer = null;
